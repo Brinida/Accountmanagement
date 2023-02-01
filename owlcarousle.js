f3151705e35dcd228869ad8c97f817d47671b9e9
Brinida <subedibrinita@gmail.com>
2023-02-01 17:13:02 +0545

    modified

---
+++ owlcarousle.js
@@ -0,0 +1,32 @@
+jQuery("#carousel").owlCarousel({
+    autoplay: true,
+    loop: true, /* use rewind if you don't want loop */
+    margin: 20,
+     /*
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    */
+    responsiveClass: true,
+    autoHeight: true,
+
+    autoplayTimeout: 4000,
+    smartSpeed: 600,
+  
+    responsive: {
+      0: {
+        items: 1
+      },
+  
+      600: {
+        items: 2
+      },
+  
+      1024: {
+        items: 3
+      },
+  
+      1366: {
+        items: 3
+      }
+    }
+  });
